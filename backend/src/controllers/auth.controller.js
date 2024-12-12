a5d93c2a01e0ae41333e06016fe0f9a268e2a19a
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2024-12-12 20:54:59 +0530

    Complete SignUp, Login, Logout and Profile Manage functionalities. Also, add the theme change functions into settings page

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -98,23 +98,23 @@ export const logout = (req, res) => {
 export const updateProfile = async (req, res) => {
   try {
     const { profilePic } = req.body;
-    const userid = req.user._id;
+    const userId = req.user._id;
 
     if (!profilePic) {
-      return res.status(400).json({ message: "Profile Pic is required" });
+      return res.status(400).json({ message: "Profile pic is required" });
     }
 
     const uploadResponse = await cloudinary.uploader.upload(profilePic);
     const updatedUser = await User.findByIdAndUpdate(
-      userid,
+      userId,
       { profilePic: uploadResponse.secure_url },
       { new: true }
     );
 
     res.status(200).json(updatedUser);
   } catch (error) {
-    console.log("Error in updateProfile controller", error.message);
-    res.status(500).json({ message: "Internal Server error" });
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
+
+// routes
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -12,8 +16,15 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow cookies to be sent back and forth between client and server
+  })
+);
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
